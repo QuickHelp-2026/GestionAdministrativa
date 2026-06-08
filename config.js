47d3a3b44bbea64a3665f38976be1016cb8eaf3d
QuickHelp-2026 <quickhelp.web2025@gmail.com>
2026-06-08 12:41:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
 // Déjala vacía ('') para usar datos de demostración locales.
 // ============================================================
 
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxcympZSMjfSz4NSBTTYXyU8jotXL8t6lrI6cPB5TpzxzDSNjzsCXvfVb04svOX0AwR/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbw1JmRJf02jrKXvsTzvjN9L20LRJaJnVMJY5qG0jDtylocvwlsRN8tPFlqisBp0lR_L/exec';
